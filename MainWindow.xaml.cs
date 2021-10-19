a3d043268bfc7057a3d720dfb0b76431bf5614a7
Shir0Yaksha <85480331+Sh1r0Yaksha@users.noreply.github.com>
2021-10-19 09:37:36 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Numbers Numbers { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Numbers = new Numbers { Num1 = "0", Num2 = "0" };
+            this.DataContext = Numbers;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Numbers.result = double.Parse(Numbers.Num1) + double.Parse(Numbers.Num2);
+            Numbers.Result = Numbers.result.ToString();
+        }
+
+        private void SubtractButton_Click(object sender, RoutedEventArgs e)
+        {
+            Numbers.result = double.Parse(Numbers.Num1) - double.Parse(Numbers.Num2);
+            Numbers.Result = Numbers.result.ToString();
+        }
+
+        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Numbers.result = double.Parse(Numbers.Num1) * double.Parse(Numbers.Num2);
+            Numbers.Result = Numbers.result.ToString();
+        }
+
+        private void DivideButton_Click(object sender, RoutedEventArgs e)
+        {
+            Numbers.result = double.Parse(Numbers.Num1) / double.Parse(Numbers.Num2);
+            Numbers.Result = Numbers.result.ToString();
+        }
+    }
+}
---
+++ Numbers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Calculator
+{
+    class Numbers : INotifyPropertyChanged
+    {
+        private double num1;
+        private double num2;
+        public double result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Num1
+        {
+            get
+            {
+                return num1.ToString();
+            }
+            set
+            {
+                double number;
+                bool parsed = double.TryParse(value, out number);
+                if (parsed)
+                {
+                    num1 = number;
+                }
+                OnPropertyChanged("Num1");
+                OnPropertyChanged("Result");
+            }
+        }
+        public string Num2
+        {
+            get
+            {
+                return num2.ToString();
+            }
+            set
+            {
+                double number;
+                bool parsed = double.TryParse(value, out number);
+                if (parsed)
+                {
+                    num2 = number;
+                }
+                OnPropertyChanged("Num2");
+                OnPropertyChanged("Result");
+            }
+        }
+        public string Result 
+        {
+            get
+            {
+                return result.ToString();
+            }
+            set
+            {
+                double number;
+                bool parsed = double.TryParse(value, out number);
+                result = number;
+                OnPropertyChanged("Result");
+            }
+        }
+    }
+}
